4079f8a985e31ac1e3fd9d60b54d077b3b3d6601
anassss10 <anassss2512@gmail.com>
2025-11-19 12:06:39 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -19,6 +19,11 @@ const user = new mongoose.Schema({
         type: String,
         required: true
     },
+     mobile: {
+        type: String,
+        required: true,
+        unique: true
+    },
    avatar : {
     type: String,
     default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
